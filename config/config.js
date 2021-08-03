fb4a42e8125a9a9725dabc3c0e968ec38c576c15
vijethvij <56092770+vijethvij@users.noreply.github.com>
2021-08-04 00:00:11 +0530

    Edited auth function

--- config/config.js
+++ config/config.js
@@ -4,6 +4,6 @@ const PORT = process.env.PORT || 4000;
 //mongo URI
 const MONGOURI = "mongodb+srv://teammember1:teammember1@clusterhireease.0wu2q.mongodb.net/test?retryWrites=true&w=majority";
 
-const ADMINAUTHID = "MCEPlacementCell@Neels";
+const ADMINAUTHID = "PlacementCell";
 
 module.exports = {PORT : PORT, MONGOURI : MONGOURI, ADMINAUTHID : ADMINAUTHID };

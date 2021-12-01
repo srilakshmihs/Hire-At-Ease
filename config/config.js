4fe3af72c97c030fbb8ffa68c90f089c25020589
vijethvij <56092770+vijethvij@users.noreply.github.com>
2021-12-01 20:30:53 +0530

    Updated Config.js

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@
 const PORT = process.env.PORT || 4000;
 
 //mongo URI
-const MONGOURI = "mongodb+srv://teammember1:teammember1@clusterhireease.0wu2q.mongodb.net/test?retryWrites=true&w=majority";
+const MONGOURI = "mongodb+srv://teammembeteammember1:r1@clusterhireease.0wu2q.mongodb.net/test?retryWrites=true&w=majority";
 
 const ADMINAUTHID = "PlacementCell";
 
